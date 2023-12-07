2810dee6d668d44e1f17c7376af61ca498966a55
Abdullah78678 <147414737+Abdullah78678@users.noreply.github.com>
2023-12-07 18:44:26 +0000

    Add files via upload

---
+++ service.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function (){
+    var currentUrl = window.location.href;
+    var navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(function (link){
+        if (link.href ===currentUrl){
+            link.classList.add('active');
+        }
+    });
+});
